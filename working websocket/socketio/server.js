50e8c2e8103df5971684409d0ba261340bad1f36
Frank-Johnson7681 <116653043+Frank-Johnson7681@users.noreply.github.com>
2024-09-23 17:23:54 -0400

    removed HTML code and express

--- working websocket/socketio/server.js
+++ working websocket/socketio/server.js
@@ -1,21 +1,9 @@
-var express = require('express');  // Use Express for serving files
 var http = require('http');
 var socketIo = require('socket.io');
 var os = require('os');  // Used to get local network IP addresses
 
-// Create an Express app
-var app = express();
-
-// Serve static files from the public directory
-app.use(express.static('public'));
-
 // Create an HTTP server
-var server = http.createServer(app);
-
-// Ensure that the root ('/') serves the index.html
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
-});
+var server = http.createServer();
 
 // Create Socket.IO server with CORS enabled
 const io = socketIo(server, {
@@ -44,22 +32,24 @@ server.listen(5000, function() {
     console.log((new Date()) + ' Server is listening on ' + serverAddress + ':5000');
 });
 
+// Handle WebSocket connections
 io.on('connection', (socket) => {
     console.log("Client connected");
-	 
-	socket.on('relay_control', (command) => {
+
+    socket.on('relay_control', (command) => {
         console.log('Relay control command received:', command);
 
         // Add logic here to handle relay control, e.g., sending the command to the ESP32
         if (command === "relay1_on") {
-            // Add the code to turn on the relay
             console.log("Turning Relay 1 ON");
         } else if (command === "relay1_off") {
-            // Add the code to turn off the relay
             console.log("Turning Relay 1 OFF");
         }
-		socket.broadcast.emit('relay_control', command);
-});
+
+        // Broadcast the command to other clients (like ESP32)
+        socket.broadcast.emit('relay_control', command);
+    });
+
     // Handle button states
     socket.on('button_states', (data) => {
         try {
@@ -84,3 +74,4 @@ io.on('connection', (socket) => {
         console.log('Client disconnected');
     });
 });
+
